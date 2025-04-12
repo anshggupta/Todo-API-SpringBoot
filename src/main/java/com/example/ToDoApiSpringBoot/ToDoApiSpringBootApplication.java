fca307848f5f39f7ec7ec7fe8780b287b9304239
anshggupta <anshgggupta300@gmail.com>
2025-04-12 11:13:31 +0530

    Added basic controller structure

---
+++ src/main/java/com/example/ToDoApiSpringBoot/ToDoApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ToDoApiSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ToDoApiSpringBoot/ToDoApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToDoApiSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
